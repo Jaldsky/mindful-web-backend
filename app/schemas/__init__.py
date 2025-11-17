126c7560f33dad64c59be8370a96600adcd67f39
Artem Ryzhenkov <ryzhenkov.artem2@wb.ru>
2025-11-17 14:07:05 +0300

    Delete some unnecessary ErrorCode

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,8 +1,9 @@
 from .common_meta_schema import CommonMetaSchema
-from .error_response_schema import ErrorResponseSchema, ErrorCode
+from .error_response_schema import ErrorResponseSchema, ErrorCode, ErrorDetailData
 
 __all__ = (
     "CommonMetaSchema",
     "ErrorResponseSchema",
     "ErrorCode",
+    "ErrorDetailData",
 )
