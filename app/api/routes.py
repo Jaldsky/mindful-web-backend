cd0bf79522d41e2b3a2f10502e055cfb4bf18ab2
Artem Ryzhenkov <ryzhenkov.artem2@wb.ru>
2025-11-17 14:17:20 +0300

    Add send events 405 handler + add tests for handlers

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,5 +1,8 @@
 """Константы путей API endpoints."""
 
 API_V1_PREFIX = "/api/v1"
+
 HEALTHCHECK_PATH = f"{API_V1_PREFIX}/healthcheck"
 EVENTS_PATH = f"{API_V1_PREFIX}/events"
+
+SEND_EVENTS_PATH = f"{EVENTS_PATH}/send"
