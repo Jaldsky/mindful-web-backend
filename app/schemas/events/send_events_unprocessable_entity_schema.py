0dbbb225348f5c8a168e1d8105898b46bba77d1d
Artem Ryzhenkov <ryzhenkov.artem2@wb.ru>
2025-11-18 13:35:38 +0300

    Add support 422 code for events endpoint

--- app/schemas/events/send_events_unprocessable_entity_schema.py
+++ app/schemas/events/send_events_unprocessable_entity_schema.py
@@ -5,25 +5,25 @@ from ..error_response_schema import ErrorResponseSchema, ErrorCode
 from .event_error_code import EventsErrorCode
 
 
-class SendEventsBadRequestSchema(ErrorResponseSchema):
-    """Схема ошибки 400 Bad Request для events endpoint."""
+class SendEventsUnprocessableEntitySchema(ErrorResponseSchema):
+    """Схема ошибки 422 Unprocessable Entity для events endpoint."""
 
     code: Union[ErrorCode | EventsErrorCode] = Field(..., description="Код ошибки")
 
     class Config:
         json_schema_extra = {
             "example": {
-                "code": "VALIDATION_ERROR",
-                "message": "Payload validation failed",
+                "code": "INVALID_EVENT_TYPE",
+                "message": "Event must be either active or inactive",
                 "details": [
                     {
-                        "field": "data[].timestamp",
-                        "message": "Input should be a valid datetime, invalid character detected",
-                        "value": "my-str",
+                        "field": "data[].event",
+                        "message": "Event must be either active or inactive",
+                        "value": "focus",
                     }
                 ],
                 "meta": {
-                    "request_id": "aaa5c2b3-1a1b-4612-8ad9-e9e731ea82c0",
+                    "request_id": "5d4c75de-7d7d-4f2d-a86f-4d6d00d2d2f7",
                     "timestamp": "2025-11-15T08:38:01.961050Z",
                 },
             }
