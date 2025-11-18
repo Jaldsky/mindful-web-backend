af0832cb7f9b5f7857c13e441cef10a820ce6ed7
Artem Ryzhenkov <ryzhenkov.artem2@wb.ru>
2025-11-18 20:04:45 +0300

    Add service_unavailable_handler for healthcheck

--- app/services/healthcheck/__init__.py
+++ app/services/healthcheck/__init__.py
@@ -1,3 +1,9 @@
-from .http_handler import healthcheck_method_not_allowed_response
+from .http_handler import (
+    healthcheck_method_not_allowed_response,
+    healthcheck_service_unavailable_response,
+)
 
-__all__ = ("healthcheck_method_not_allowed_response",)
+__all__ = (
+    "healthcheck_method_not_allowed_response",
+    "healthcheck_service_unavailable_response",
+)
