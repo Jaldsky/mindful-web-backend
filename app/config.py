1de31437ac299f253805e36a1e6b509351ca5169
Artem Ryzhenkov <ryzhenkov.artem2@wb.ru>
2025-11-30 16:32:48 +0300

    Add SMTP deploy + libs

--- app/config.py
+++ app/config.py
@@ -7,11 +7,14 @@ DATE_FORMATS = [
 
 DEFAULT_PAGE_SIZE: int = 20
 
-POSTGRES_USER: str = os.getenv("POSTGRES_USER", "root")
-POSTGRES_PASSWORD: str = os.getenv("POSTGRES_PASSWORD", "root")
+CORS_ALLOW_ORIGINS: list[str] = os.getenv("CORS_ALLOW_ORIGINS", "chrome-extension://*").split(",")
+
+# Database
 POSTGRES_HOST: str = os.getenv("POSTGRES_HOST", "mwb-db")
 POSTGRES_PORT: str = os.getenv("POSTGRES_PORT", 5432)
 POSTGRES_DB: str = os.getenv("POSTGRES_DB", "mwb")
+POSTGRES_USER: str = os.getenv("POSTGRES_USER", "root")
+POSTGRES_PASSWORD: str = os.getenv("POSTGRES_PASSWORD", "root")
 
 DATABASE_ASYNC_URL: str = os.getenv(
     "DATABASE_ASYNC_URL",
@@ -22,11 +25,20 @@ DATABASE_SYNC_URL: str = os.getenv(
     f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}",
 )
 
-REDIS_PASSWORD: str = os.getenv("REDIS_PASSWORD", "root")
+# Redis
 REDIS_HOST: str = os.getenv("REDIS_HOST", "mwb-redis")
 REDIS_PORT: str = os.getenv("REDIS_PORT", 6379)
 REDIS_DB: str = os.getenv("REDIS_DB", 0)
+REDIS_PASSWORD: str = os.getenv("REDIS_PASSWORD", "root")
 
 REDIS_URL: str = os.getenv("REDIS_URL", f"redis://:{REDIS_PASSWORD}@{REDIS_HOST}:{REDIS_PORT}/{REDIS_DB}")
 
-CORS_ALLOW_ORIGINS: list[str] = os.getenv("CORS_ALLOW_ORIGINS", "chrome-extension://*").split(",")
+# Email Configuration
+SMTP_HOST: str = os.getenv("SMTP_HOST", "mwb-mailhog")
+SMTP_PORT: int = os.getenv("SMTP_PORT", 1025)
+SMTP_USER: str = os.getenv("SMTP_USER", "root")
+SMTP_PASSWORD: str = os.getenv("SMTP_PASSWORD", "root")
+
+SMTP_FROM_EMAIL: str = os.getenv("SMTP_FROM_EMAIL", "noreply@mindful-web.com")
+SMTP_FROM_NAME: str = os.getenv("SMTP_FROM_NAME", "Mindful-Web")
+SMTP_USE_TLS: bool = os.getenv("SMTP_USE_TLS", "false").lower() == "true"
